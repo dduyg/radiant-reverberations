5f6c311939a127a559bfb60e3286636ab306e771
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-11-22 22:26:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function draw() {
   background(235, 255, 160);
 
   const div = pow(2, floor((frameCount % 160) / 40)) * 8;
+
   const sampleDelta = PI / div;
   const nrSamples = 0.0;
 
@@ -18,18 +19,26 @@ function draw() {
   noStroke();
   fill(0);
 
+  // Draw upper hemisphere
   for (let phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
-    for (let theta = 0.0; theta < PI; theta += sampleDelta) {
+    for (let theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
       const x = sin(theta) * cos(phi);
       const y = sin(theta) * sin(phi);
       const z = cos(theta);
 
-      // Upper sphere
       circle(x * radius + cx, cy - (z - y * 0.25) * radius, 2);
       nSamples++;
+    }
+  }
 
-      // Lower sphere (mirrored)
-      circle(x * radius + cx, cy + (z - y * 0.25) * radius, 2);
+  // Draw lower hemisphere (mirrored)
+  for (let phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
+    for (let theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
+      const x = sin(theta) * cos(phi);
+      const y = sin(theta) * sin(phi);
+      const z = -cos(theta); // Reverse the sign for mirroring
+
+      circle(x * radius + cx, cy - (z - y * 0.25) * radius, 2);
       nSamples++;
     }
   }
