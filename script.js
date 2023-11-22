ff3605e16cde26ef4e0a8022ea17387c44ae8d9d
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-11-22 22:20:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 function setup() {
-	const canvas = createCanvas(windowWidth, windowHeight);
+  const canvas = createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background('#d1d6e6');
+  background(235, 255, 160);
 
-  
-  const div = pow(2, floor((frameCount % 180) / 40)) * 8;
-  
+  const div = pow(2, floor((frameCount % 160) / 40)) * 8;
   const sampleDelta = PI / div;
-  const nrSampes = 0.0;
-  
+  const nrSamples = 0.0;
+
   const radius = min(width, height) * 0.4;
   const cx = width / 2;
   const cy = height / 2 + radius * 0.5;
-  
+
   let nSamples = 0;
-  
-  
+
   noStroke();
   fill(0);
+
   for (let phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
-    for (let theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
+    for (let theta = 0.0; theta < PI; theta += sampleDelta) {
       const x = sin(theta) * cos(phi);
       const y = sin(theta) * sin(phi);
       const z = cos(theta);
-      
+
+      // Upper sphere
       circle(x * radius + cx, cy - (z - y * 0.25) * radius, 2);
-    nSamples ++;
+      nSamples++;
+
+      // Lower sphere (mirrored)
+      circle(x * radius + cx, cy + (z - y * 0.25) * radius, 2);
+      nSamples++;
     }
   }
-  
+
   drawLabel(8, 32, "Number of samples " + nSamples, LEFT);
 }
 
@@ -40,8 +43,12 @@ function drawLabel(x, y, label, align = CENTER) {
   textFont("monospace");
   textSize(14);
   textAlign(align);
-  if (align == LEFT) {x += 6;}
-  if (align == RIGHT) {x -= 6;}
+  if (align == LEFT) {
+    x += 6;
+  }
+  if (align == RIGHT) {
+    x -= 6;
+  }
   text(label, x, y);
   pop();
 }
