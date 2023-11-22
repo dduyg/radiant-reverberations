6135e3878a36f849a4c14375c1038d923d83a455
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-11-22 23:08:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,44 +6,42 @@ function draw() {
   background(235, 255, 160);
 
   const div = pow(2, floor((frameCount % 160) / 40)) * 8;
-
   const sampleDelta = PI / div;
-  const nrSamples = 0.0;
-
+  
   const radius = min(width, height) * 0.4;
-  const cx = width / 2;
+  const cx = width / 4;
   const cy = height / 2 + radius * 0.5;
-
+  
   let nSamples = 0;
+  
+  // Draw the first sphere
+  drawSphere(cx, cy, radius, sampleDelta);
+  
+  // Draw the second sphere (mirrored)
+  drawSphere(3 * cx, cy, radius, sampleDelta, true);
+  
+  drawLabel(8, 32, "Number of samples " + nSamples, LEFT);
+}
 
+function drawSphere(cx, cy, radius, sampleDelta, mirrored = false) {
   noStroke();
   fill(0);
-
-  // Draw upper hemisphere
   for (let phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
     for (let theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
       const x = sin(theta) * cos(phi);
       const y = sin(theta) * sin(phi);
       const z = cos(theta);
 
-      circle(x * radius + cx, cy - (z - y * 0.25) * radius, 2);
+      // Mirror the second sphere
+      if (mirrored) {
+        circle(cx - x * radius, cy - (z - y * 0.25) * radius, 2);
+      } else {
+        circle(x * radius + cx, cy - (z - y * 0.25) * radius, 2);
+      }
+
       nSamples++;
     }
   }
-
-  // Draw lower hemisphere (mirrored)
-  for (let phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
-    for (let theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
-      const x = sin(theta) * cos(phi);
-      const y = sin(theta) * sin(phi);
-      const z = -cos(theta); // Reverse the sign for mirroring
-
-      circle(x * radius + cx, cy - (z - y * 0.25) * radius, 2);
-      nSamples++;
-    }
-  }
-
-  drawLabel(8, 32, "Number of samples " + nSamples, LEFT);
 }
 
 function drawLabel(x, y, label, align = CENTER) {
@@ -52,12 +50,8 @@ function drawLabel(x, y, label, align = CENTER) {
   textFont("monospace");
   textSize(14);
   textAlign(align);
-  if (align == LEFT) {
-    x += 6;
-  }
-  if (align == RIGHT) {
-    x -= 6;
-  }
+  if (align == LEFT) {x += 6;}
+  if (align == RIGHT) {x -= 6;}
   text(label, x, y);
   pop();
 }
