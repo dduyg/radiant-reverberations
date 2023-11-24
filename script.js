8e2d2c8e41938588b183b8d20343f1a665af5d6e
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-11-24 12:23:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function draw() {
   background('#d1d6e6');
 
   // Control the pattern parameters with frameCount
-  const div = pow(2, floor((frameCount % 200) / 40)) * 7;
+  const div = pow(2, floor((frameCount % 200) / 40)) * 8;
 
   // Calculate the angular step for sampling
   const sampleDelta = PI / div;
