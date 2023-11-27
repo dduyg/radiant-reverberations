0f90d076817af2bc0d817918158ceceaecec1278
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-11-27 23:17:47 +0100

    Update main.js

--- main.js
+++ main.js
@@ -85,16 +85,3 @@ function drawLabel(x, y, label, value, align = CENTER) {
   
   pop();
 }
-
-/***********************************
- *           infoDropdown          *
- ***********************************/
-function toggleInfo() {
-  const dropdown = document.getElementById("infoDropdown");
-  dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
-}
-
-function closeInfo() {
-  document.getElementById("infoDropdown").style.display = "none";
-}
-/***********************************/
