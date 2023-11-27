1b6cbf9feed2596edd35b2af993fe09a9b05599e
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-11-27 20:49:16 +0100

    Update 45.js

--- 45.js
+++ 45.js
@@ -1,4 +1,4 @@
-// Define simulation constants
+// Set constants for canvas size, frame count modification, sample division, sphere properties, and positioning
 const CANVAS_PERCENTAGE = 0.9;
 const FRAME_MODIFIER = 200;
 const SAMPLE_DIVIDER = 40;
@@ -7,98 +7,76 @@ const VERTICAL_SEPARATION_PERCENTAGE = 0.3;
 const CENTER_Y_ADJUSTMENT = 0.9;
 const Y_OFFSET_PERCENTAGE = 0.3;
 
-// Setup function to initialize the canvas
+// Setup function to create canvas
 function setup() {
-  createSimulationCanvas();
+  const canvas = createCanvas(windowWidth * CANVAS_PERCENTAGE, windowHeight * CANVAS_PERCENTAGE);
 }
 
-// Draw function to render the simulation
+// Draw function for the animation
 function draw() {
   // Set background color
   background('#d1d6e6');
 
-  // Calculate the number of sample divisions based on frame count
-  const sampleDivisions = calculateSampleDivisions();
+  // Calculate sample divisions based on frame count
+  const div = pow(2, floor((frameCount % FRAME_MODIFIER) / SAMPLE_DIVIDER)) * 9;
+  const sampleDelta = PI / div; // Angle between samples
 
-  // Draw samples on the upper hemisphere
-  drawHemisphereSamples(sampleDivisions, height / 2 - min(width, height) * SPHERE_RADIUS_PERCENTAGE * CENTER_Y_ADJUSTMENT);
-
-  // Draw samples on the lower hemisphere
-  drawHemisphereSamples(sampleDivisions, height / 2 + min(width, height) * SPHERE_RADIUS_PERCENTAGE * (1 + VERTICAL_SEPARATION_PERCENTAGE));
-
-  // Display the number of samples
-  drawLabel(8, 32, "Number of samples ", countSamples(sampleDivisions), LEFT);
-}
-
-// Function to create the canvas with a percentage of the window size
-function createSimulationCanvas() {
-  try {
-    createCanvas(windowWidth * CANVAS_PERCENTAGE, windowHeight * CANVAS_PERCENTAGE);
-  } catch (error) {
-    console.error("Canvas creation failed:", error.message);
-  }
-}
-
-// Function to calculate the number of sample divisions based on frame count
-function calculateSampleDivisions() {
-  return pow(2, floor((frameCount % FRAME_MODIFIER) / SAMPLE_DIVIDER)) * 9;
-}
-
-// Function to draw samples on a hemisphere
-function drawHemisphereSamples(sampleDivisions, cy) {
-  const sampleDelta = PI / sampleDivisions;
+  // Initialize variables
   let nSamples = 0;
-
-  // Calculate properties for the hemisphere
   const radius = min(width, height) * SPHERE_RADIUS_PERCENTAGE;
   const cx = width / 2;
+  const cy = height / 2 - radius * CENTER_Y_ADJUSTMENT;
 
-  noStroke();
-  fill(0);
-
-  // Loop through phi and theta to draw samples
+  // Draw samples on the upper hemisphere
   for (let phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
     for (let theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
-      // Draw a sample point and update the sample count
-      nSamples = drawSamplePoint(phi, theta, radius, cx, cy, sampleDelta, nSamples);
+      const x = sin(theta) * cos(phi);
+      const y = sin(theta) * sin(phi);
+      const z = cos(theta);
+
+      circle(x * radius + cx, cy + (z - y * 0.25) * radius, 2);
+      nSamples++;
     }
   }
+
+  // Draw samples on the lower hemisphere with vertical separation
+  const yOffset = height * Y_OFFSET_PERCENTAGE;
+  for (let phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
+    for (let theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
+      const x = sin(theta) * cos(phi);
+      const y = sin(theta) * sin(phi);
+      const z = cos(theta);
+
+      circle(x * radius + cx, cy + radius + yOffset - (z + y * 0.25) * radius, 2);
+      nSamples++;
+    }
+  }
+
+  // Display the number of samples
+  drawLabel(8, 32, "Number of samples ", nSamples, LEFT);
 }
 
-// Function to draw a sample point and return the updated sample count
-function drawSamplePoint(phi, theta, radius, cx, cy, sampleDelta, nSamples) {
-  // Convert spherical coordinates to Cartesian coordinates
-  const x = sin(theta) * cos(phi);
-  const y = sin(theta) * sin(phi);
-  const z = cos(theta);
-
-  // Draw a sample point
-  circle(x * radius + cx, cy + (z - y * VERTICAL_SEPARATION_PERCENTAGE) * radius, 2);
-
-  // Increment the sample count
-  return nSamples + 1;
-}
-
-// Function to display a label with a value
+// Function to draw labels with specified position and alignment
 function drawLabel(x, y, label, value, align = CENTER) {
   push();
   strokeWeight(0);
   textFont("monospace");
   textSize(15);
   textAlign(align);
+
+  // Adjust label position for LEFT and RIGHT alignment
   if (align == LEFT) {
     x += 6;
   }
   if (align == RIGHT) {
     x -= 6;
   }
-  
-  // Draw the label in green
+
+  // Set label color and draw
   fill('#01af52');
   text(label, x, y + 45);
-  // Draw the value in black
   fill(0);
-  text(value, x + textWidth(label + ' '), y + 45);
+  text(value, x + textWidth(label + ' '), y + 45); // Adjust the value to move the label lower/higher
   
   pop();
 }
