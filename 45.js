43794a36f1f7daeb35d27a9cc13220b228ed0c3f
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-11-27 19:16:01 +0100

    Create 45.js

---
+++ 45.js
@@ -0,0 +1,104 @@
+// Define simulation constants
+const CANVAS_PERCENTAGE = 0.9;
+const FRAME_MODIFIER = 200;
+const SAMPLE_DIVIDER = 40;
+const SPHERE_RADIUS_PERCENTAGE = 0.4;
+const VERTICAL_SEPARATION_PERCENTAGE = 0.3;
+const CENTER_Y_ADJUSTMENT = 0.9;
+const Y_OFFSET_PERCENTAGE = 0.3;
+
+// Setup function to initialize the canvas
+function setup() {
+  createSimulationCanvas();
+}
+
+// Draw function to render the simulation
+function draw() {
+  // Set background color
+  background('#d1d6e6');
+
+  // Calculate the number of sample divisions based on frame count
+  const sampleDivisions = calculateSampleDivisions();
+
+  // Draw samples on the upper hemisphere
+  drawHemisphereSamples(sampleDivisions, height / 2 - min(width, height) * SPHERE_RADIUS_PERCENTAGE * CENTER_Y_ADJUSTMENT);
+
+  // Draw samples on the lower hemisphere
+  drawHemisphereSamples(sampleDivisions, height / 2 + min(width, height) * SPHERE_RADIUS_PERCENTAGE * (1 + VERTICAL_SEPARATION_PERCENTAGE));
+
+  // Display the number of samples
+  drawLabel(8, 32, "Number of samples ", countSamples(sampleDivisions), LEFT);
+}
+
+// Function to create the canvas with a percentage of the window size
+function createSimulationCanvas() {
+  try {
+    createCanvas(windowWidth * CANVAS_PERCENTAGE, windowHeight * CANVAS_PERCENTAGE);
+  } catch (error) {
+    console.error("Canvas creation failed:", error.message);
+  }
+}
+
+// Function to calculate the number of sample divisions based on frame count
+function calculateSampleDivisions() {
+  return pow(2, floor((frameCount % FRAME_MODIFIER) / SAMPLE_DIVIDER)) * 9;
+}
+
+// Function to draw samples on a hemisphere
+function drawHemisphereSamples(sampleDivisions, cy) {
+  const sampleDelta = PI / sampleDivisions;
+  let nSamples = 0;
+
+  // Calculate properties for the hemisphere
+  const radius = min(width, height) * SPHERE_RADIUS_PERCENTAGE;
+  const cx = width / 2;
+
+  noStroke();
+  fill(0);
+
+  // Loop through phi and theta to draw samples
+  for (let phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
+    for (let theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
+      // Draw a sample point and update the sample count
+      nSamples = drawSamplePoint(phi, theta, radius, cx, cy, sampleDelta, nSamples);
+    }
+  }
+}
+
+// Function to draw a sample point and return the updated sample count
+function drawSamplePoint(phi, theta, radius, cx, cy, sampleDelta, nSamples) {
+  // Convert spherical coordinates to Cartesian coordinates
+  const x = sin(theta) * cos(phi);
+  const y = sin(theta) * sin(phi);
+  const z = cos(theta);
+
+  // Draw a sample point
+  circle(x * radius + cx, cy + (z - y * VERTICAL_SEPARATION_PERCENTAGE) * radius, 2);
+
+  // Increment the sample count
+  return nSamples + 1;
+}
+
+// Function to display a label with a value
+function drawLabel(x, y, label, value, align = CENTER) {
+  push();
+  strokeWeight(0);
+  textFont("monospace");
+  textSize(15);
+  textAlign(align);
+  if (align == LEFT) {
+    x += 6;
+  }
+  if (align == RIGHT) {
+    x -= 6;
+  }
+  
+  // Draw the label in green
+  fill('#01af52');
+  text(label, x, y + 45);
+  // Draw the value in black
+  fill(0);
+  text(value, x + textWidth(label + ' '), y + 45);
+  
+  pop();
+}
