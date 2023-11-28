7561da0f591f74d7a466dfc2b226a3284e922e97
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-11-28 01:43:03 +0100

    Update 45.js

--- 45.js
+++ 45.js
@@ -1,4 +1,4 @@
-// Set constants for canvas size, frame count modification, sample division, sphere properties, and positioning
+// Constants for adjusting various parameters
 const CANVAS_PERCENTAGE = 0.9;
 const FRAME_MODIFIER = 200;
 const SAMPLE_DIVIDER = 40;
@@ -7,88 +7,78 @@ const VERTICAL_SEPARATION_PERCENTAGE = 0.3;
 const CENTER_Y_ADJUSTMENT = 0.9;
 const Y_OFFSET_PERCENTAGE = 0.3;
 
-// Setup function to initialize the canvas
+// Setup function to create the canvas
 function setup() {
-  createSimulationCanvas();
-}
-
-// Draw function to render the simulation
-function draw() {
-  background('#d1d6e6');
-
-  // Calculate sample divisions based on frame count
-  const sampleDivisions = calculateSampleDivisions();
-
-  // Draw samples on the upper hemisphere
-  drawHemisphereSamples(sampleDivisions, height / 2 - min(width, height) * SPHERE_RADIUS_PERCENTAGE * CENTER_Y_ADJUSTMENT);
-
-  // Draw samples on the lower hemisphere
-  drawHemisphereSamples(sampleDivisions, height / 2 + min(width, height) * SPHERE_RADIUS_PERCENTAGE * (1 + VERTICAL_SEPARATION_PERCENTAGE));
-
-  // Display the number of samples
-  drawLabel(8, 32, "Number of samples ", countSamples(sampleDivisions), LEFT);
-}
-
-// Function to create the canvas with a percentage of the window size
-function createSimulationCanvas() {
   try {
-    createCanvas(windowWidth * CANVAS_PERCENTAGE, windowHeight * CANVAS_PERCENTAGE);
+    // Create a canvas with a responsive size
+    createResponsiveCanvas();
   } catch (error) {
-    console.error("Canvas creation failed:", error.message);
+    console.error("Error creating canvas:", error.message);
   }
 }
 
-// Function to calculate the number of sample divisions based on frame count
-function calculateSampleDivisions() {
-  return pow(2, floor((frameCount % FRAME_MODIFIER) / SAMPLE_DIVIDER)) * 9;
+// Function to create a responsive canvas
+function createResponsiveCanvas() {
+  const canvas = createCanvas(windowWidth * CANVAS_PERCENTAGE, windowHeight * CANVAS_PERCENTAGE);
+  if (!canvas) {
+    throw new Error("Canvas creation failed.");
+  }
+}
+
+// Draw function for the animation
+function draw() {
+  // Set background color
+  background('#d1d6e6');
+
+  // Calculate the number of samples based on frame count
+  const sampleDivisions = pow(2, floor((frameCount % FRAME_MODIFIER) / SAMPLE_DIVIDER)) * 9;
+  const sampleDelta = PI / sampleDivisions;
+  let totalSamples = 0;
+
+  // Calculate sphere properties
+  const sphereRadius = min(width, height) * SPHERE_RADIUS_PERCENTAGE;
+  const centerX = width / 2;
+  const centerY = height / 2 - sphereRadius * CENTER_Y_ADJUSTMENT;
+
+  // Draw samples on the upper hemisphere
+  drawHemisphere(centerX, centerY, sphereRadius, sampleDelta, totalSamples);
+  
+  // Offset for the lower hemisphere
+  const yOffset = height * Y_OFFSET_PERCENTAGE;
+
+  // Draw samples on the lower hemisphere
+  drawHemisphere(centerX, centerY + sphereRadius + yOffset, sphereRadius, sampleDelta, totalSamples);
+
+  // Display the number of samples
+  drawLabel(8, 32, "Number of samples", totalSamples, LEFT);
 }
 
 // Function to draw samples on a hemisphere
-function drawHemisphereSamples(sampleDivisions, cy) {
-  const sampleDelta = PI / sampleDivisions;
-  let nSamples = 0;
-
-  // Calculate properties for the hemisphere
-  const radius = min(width, height) * SPHERE_RADIUS_PERCENTAGE;
-  const cx = width / 2;
-
+function drawHemisphere(centerX, centerY, radius, delta, totalSamples) {
   noStroke();
   fill(0);
 
-  // Loop through phi and theta to draw samples
-  for (let phi = 0.0; phi < 2.0 * PI; phi += sampleDelta) {
-    for (let theta = 0.0; theta < 0.5 * PI; theta += sampleDelta) {
-      // Draw a sample point and update the sample count
-      nSamples = drawSamplePoint(phi, theta, radius, cx, cy, sampleDelta, nSamples);
+  for (let phi = 0.0; phi < 2.0 * PI; phi += delta) {
+    for (let theta = 0.0; theta < 0.5 * PI; theta += delta) {
+      // Convert spherical coordinates to Cartesian coordinates
+      const x = sin(theta) * cos(phi);
+      const y = sin(theta) * sin(phi);
+      const z = cos(theta);
+
+      // Adjust and draw a sample point on the hemisphere
+      circle(x * radius + centerX, centerY + (z - y * 0.25) * radius, 2);
+      totalSamples++;
     }
   }
 }
 
-// Function to draw a sample point and return the updated sample count
-function drawSamplePoint(phi, theta, radius, cx, cy, sampleDelta, nSamples) {
-  // Convert spherical coordinates to Cartesian coordinates
-  const x = sin(theta) * cos(phi);
-  const y = sin(theta) * sin(phi);
-  const z = cos(theta);
-
-  // Draw a sample point
-  circle(x * radius + cx, cy + (z - y * VERTICAL_SEPARATION_PERCENTAGE) * radius, 2);
-
-  // Increment the sample count
-  return nSamples + 1;
-}
-
-// Display the sample number label with specified position, label text, value, and alignment
+// Function to draw labels on the canvas
 function drawLabel(x, y, label, value, align = CENTER) {
   push();
-  
-  // Set the drawing style for the label
   strokeWeight(0);
   textFont("monospace");
   textSize(15);
   textAlign(align);
-
-  // Adjust label position for LEFT and RIGHT alignment
   if (align == LEFT) {
     x += 6;
   }
@@ -96,13 +86,16 @@ function drawLabel(x, y, label, value, align = CENTER) {
     x -= 6;
   }
   
-  // Set label color and draw the label
   fill('#01af52');
-  text(label, x, y + 45); // Adjust value to position the label
-  
-  // Draw the dynamic value with default black color
+  text(label, x, y + 45);
   fill(0);
-  text(value, x + textWidth(label + ' '), y + 45); // Adjust value to position the label
+  text(value, x + textWidth(label + ' '), y + 45);
   
   pop();
+}
+
+// Function to handle window resizing
+function windowResized() {
+  resizeCanvas(windowWidth * CANVAS_PERCENTAGE, windowHeight * CANVAS_PERCENTAGE);
+  redraw();
 }
