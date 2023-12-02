17cd8407689a19c7fa0c1a2591b4570e4327ada8
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-02 02:59:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -125,3 +125,21 @@ function drawLabel(x, y, label, value, align = CENTER) {
 function windowResized() {
   resizeCanvas(windowWidth * canvasPercentage, windowHeight * canvasPercentage);
 }
+
+
+// ------------------ Displaying title work ------------------
+function drawTitle(title, yOffset) {
+  push();
+  textFont("Space Mono");
+  textSize(width < 600 ? 24 : 40);
+  textAlign(CENTER);
+  fill('#01af52');
+  text(title, width / 2, height + yOffset);
+  pop();
+}
+
+// Adjust value to increase/decrease separation between visual 
+// and title; currently positioned 40px below visual
+drawTitle("Radiant Reverberations", 40);
+
+// -----------------------------------------------------------
