45df56a48c6d39580eb01d3450ec0fde792b0970
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-02 01:50:25 +0100

    Create 67.js

---
+++ 67.js
@@ -0,0 +1,20 @@
+/***********************************
+ * Title-related Code
+ * ----------------------------------------------------------
+ */
+
+// Function to draw the title below the animating visual
+function drawTitle(title, yOffset) {
+  push();
+  textFont("Space Mono");
+  textSize(width < 600 ? 24 : 40);
+  textAlign(CENTER);
+  fill('#01af52');
+  text(title, width / 2, height + yOffset);
+  pop();
+}
+
+// Draw the title below the animating visual
+drawTitle("Radiant Reverberations", 40);
+
+/***********************************/
