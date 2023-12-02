ee78b10c0c2b7fdc0aac50901f1ac545ae6893d8
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-02 21:54:48 +0100

    Update Txt.js

--- Txt.js
+++ Txt.js
@@ -2,17 +2,12 @@
 function drawTitle(title, yOffset) {
   push();
   textFont("Space Mono");
-
-  // Adjust font size based on screen width using em units directly
   textSize(width < 600 ? "1.5em" : "2em"); // Adjust these values as needed
-
-  // Limit title width to 90% of screen width for smaller screens
-  const titleWidth = min(width * 0.9, textWidth(title));
-
   fill('#01af52');
   textAlign(CENTER);
 
-  // Draw the title, centered with responsive width
+  // Limit title width to 90% of screen width
+  const titleWidth = min(width * 0.9, textWidth(title));
   text(title, width / 2, height - yOffset, titleWidth);
 
   pop();
