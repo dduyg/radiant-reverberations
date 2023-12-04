7456aba16ac52b2dce38dc122aa4e156c1c3b489
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-04 14:44:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ function draw() {
   }
 
   // Display the number of samples taken in the simulation
-  drawLabel(8, 69, "Number of samples ", nSamples, LEFT);
+  drawLabel(8, 70, "Number of samples ", nSamples, LEFT);
 
   // Display title of work
   drawTitle("A Radiant Reverberation", 2);
