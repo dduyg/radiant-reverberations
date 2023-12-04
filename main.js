60b104fe27372adb55c8544939459ecc52470f69
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-04 02:34:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ function draw() {
   drawLabel(8, 32, "Number of samples ", nSamples, LEFT);
 
   // Display title of work
-  drawTitle("A Radiant Reverberation", 2);
+  drawTitle("A Radiant Reverberation", -2);
 }
 
 // Set labels with specified styling, position, and alignment
