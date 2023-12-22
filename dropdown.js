3c90f7fc3b867b8cd0e04f2543853ddec3b44783
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-22 21:29:58 +0100

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -10,3 +10,20 @@ function closeInfo() {
   document.getElementById("infoDropdown").style.display = "none";
 }
 /***********************************/
+
+
+function adjustFontSize() {
+  const title = document.querySelector('footer h1');
+  const width = window.innerWidth;
+
+  if (width >= 600) {
+    title.style.fontSize = '28px';
+  } else {
+    const newSize = map(width, 0, 600, 16, 28);
+    title.style.fontSize = `${newSize}px`;
+  }
+}
+
+// Call the function on page load and window resize
+window.onload = adjustFontSize;
+window.onresize = adjustFontSize;
