87fd7aac38d60526013bac1caa8e48aba7576aca
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-23 00:00:47 +0100

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -12,13 +12,11 @@ function closeInfo() {
 /***********************************/
 
 
-function setFooterFontSize() {
-  const title = document.getElementById("footerTitle");
-  const width = window.innerWidth;
-
-  title.style.fontSize = width < 600 ? map(width, 0, 600, 16, 28) + "px" : "28px";
+function adjustFontSize() {
+  const title = document.querySelector('h1');
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  title.style.fontSize = width < 600 ? `${map(width, 0, 600, 16, 28)}px` : '28px';
 }
 
-// Call the function initially and on window resize
-setFooterFontSize();
-window.addEventListener("resize", setFooterFontSize);
+window.addEventListener('resize', adjustFontSize);
+window.addEventListener('load', adjustFontSize);
