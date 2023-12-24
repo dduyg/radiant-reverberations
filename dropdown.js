7fa3a18e8cd3a230b96c98f1222d37bab45d9866
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-24 13:00:29 +0100

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -10,18 +10,3 @@ function closeInfo() {
   document.getElementById("infoDropdown").style.display = "none";
 }
 /***********************************/
-
-
-function adjustFooterSize() {
-  let footer = document.querySelector('footer');
-  let screenWidth = window.innerWidth;
-
-  // Adapts font size between 18 and 28 according to screen width;
-  let fontSize = screenWidth < 600 ? map(screenWidth, 0, 600, 18, 28) : 28;
-
-  footer.style.fontSize = `${fontSize}px`;
-}
-
-// Call the function initially and whenever the window is resized
-adjustFooterSize();
-window.addEventListener('resize', adjustFooterSize);
