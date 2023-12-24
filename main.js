19d403b51380cd67def7be8e71580afb3d656a9f
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-24 16:39:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let pointFillColor = 0;
 ///////////////////////////////////////////
 function setup() {
   // Canvas size percentage to make it responsive
-  const canvasPercentage = 0.9;
+  const canvasPercentage = 1.0;
   
   const cnv = createCanvas(windowWidth * canvasPercentage, windowHeight * canvasPercentage);
   cnv.id('p5-canvas');
