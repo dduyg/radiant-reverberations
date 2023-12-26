ed7d0c8a31767b44c24feee9141f8c1d9c4adb88
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-27 00:47:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -124,12 +124,13 @@ function drawLabel(x, y, title, label, value, align = CENTER) {
   // Set up the title of work
   textStyle(BOLD);
   fill('#FFFF00'); // Background color for the title
+  noStroke();
   const titleWidth = textWidth(title);
   rect(x, y + 45 - titleFontSize, titleWidth + 8, titleFontSize + 4); // Background rectangle
   fill('#01af52'); // Text color for the title
   textFont("Space Mono");
   textSize(titleFontSize);
-  text(title, x + 4, y + 45 - titleFontSize / 2); // Adjusted position
+  text(title, x + 4, y + 45); // Add a bit of padding
 
   // Add a bit more space between the first and second lines
   const verticalSpace = titleFontSize * 0.2;
