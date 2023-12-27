48632bfabcbe14473095d30213e049a234978f3f
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-27 01:42:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -123,14 +123,15 @@ function drawLabel(x, y, title, label, value, align = CENTER) {
 
   // Set up the title of work
   textStyle(BOLD);
-  const titleWidth = textWidth(title);
-  const padding = 8; // Adjust the padding as needed
   fill('#FFFF00'); // Background color for the title
-  noStroke();
-  rect(x - padding, y + 45 - titleFontSize, titleWidth + 2 * padding, titleFontSize + 4); // Background rectangle
-  fill('#01af52'); // Text color for the title
   textFont("Space Mono");
   textSize(titleFontSize);
+  const titleBounds = font.textBounds(title, x, y + 45);
+  const paddingX = 8; // Adjust the horizontal padding as needed
+  const paddingY = 4; // Adjust the vertical padding as needed
+  noStroke();
+  rect(titleBounds.x - paddingX, titleBounds.y - paddingY, titleBounds.w + 2 * paddingX, titleBounds.h + 2 * paddingY); // Background rectangle
+  fill('#01af52'); // Text color for the title
   text(title, x, y + 45); // Adjust the position based on padding
 
   // Add a bit more space between the first and second lines
