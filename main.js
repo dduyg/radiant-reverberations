7ac3eebe71454c4c378b60b8935884109c780b84
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-27 15:45:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -127,7 +127,7 @@ function drawLabel(x, y, title, label, value, align = CENTER) {
   textFont("Space Mono");
   textSize(titleFontSize);
   const titleWidth = textWidth(title);
-  const padding = 4; // Adjust the padding as needed
+  const padding = 8; // Adjust the padding as needed
   noStroke();
   rect(x - padding, y + 45 - titleFontSize + padding, titleWidth + 2 * padding, titleFontSize - padding);
   fill('#01af52'); // Text color for the title
