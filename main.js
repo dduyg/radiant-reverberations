ef018445066f2a2828f814695c72a4131ed73c74
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-28 21:53:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,68 +1,62 @@
-// Set up the canvas for the irradiance sampling display.
+// Set up canvas with p5.js
 function setup() {
   const cnv = createCanvas(windowWidth, windowHeight);
   cnv.id('p5-canvas');
 }
 
-// Define parameters for irradiance sampling and visualization.
-let frameModifier = 200; // Controls frame variation
-let sampleDensityModifier = 40; // Influences sample density
-let upperHemisphereVerticalAdjustment = 0.95; // Adjusts upper hemisphere position
-let lowerHemisphereVerticalAdjustment = 1.5; // Adjusts lower hemisphere position
-let lightSourcePosition = 0.0; // Initial position of the light source
+// Define parameters for sampling and light source
+let frameModifier = 200;
+let sampleDensityModifier = 40;
+let upperHemisphereVerticalAdjustment = 0.95;
+let lowerHemisphereVerticalAdjustment = 1.5;
+let lightSourcePosition = 0.0;
 
-// Perform irradiance sampling and visualize dynamic patterns.
+// Main drawing function
 function draw() {
   clear();
 
-  // Calculate the number of samples per frame based on modifiers.
+  // Calculate the number of samples per frame
   const samplesPerFrame = pow(2, floor((frameCount % frameModifier) / sampleDensityModifier)) * 9;
   const sampleAngularDelta = PI / samplesPerFrame;
   let nSamples = 0;
 
-  // Set the sphere's radius percentage based on window size.
+  // Set the radius of the sphere based on screen width and height
   const sphereRadiusPercentage = width < 600 ? 0.36 : 0.24;
   const sphereRadius = min(width, height) * sphereRadiusPercentage;
   const sphereCenterX = width / 2;
 
-  // Calculate positions of upper and lower hemispheres.
+  // Calculate the center positions of upper and lower hemispheres
   const upperHemisphereCenterY = height / 2 - sphereRadius * upperHemisphereVerticalAdjustment;
   const lowerHemisphereCenterY = height / 2 + sphereRadius * lowerHemisphereVerticalAdjustment;
 
-  // Visualize irradiance samples on the upper hemisphere.
-  visualizeIrradianceSamples(upperHemisphereCenterY, sphereRadius, sampleAngularDelta, true);
+  // Draw samples on the upper hemisphere
+  drawHemisphereSamples(sphereRadius, sphereCenterX, upperHemisphereCenterY, sampleAngularDelta, 1);
 
-  // Visualize irradiance samples on the lower hemisphere.
-  visualizeIrradianceSamples(lowerHemisphereCenterY, sphereRadius, sampleAngularDelta, false);
+  // Draw samples on the lower hemisphere
+  drawHemisphereSamples(sphereRadius, sphereCenterX, lowerHemisphereCenterY, sampleAngularDelta, -1);
 
-  // Move the light source position for dynamic visualization.
+  // Move the light source position for animation
   lightSourcePosition += 0.01;
 
-  // Display label with relevant information.
+  // Draw information label
   drawLabel(8, 46, "Radiant Reverberations", "Number of samples: ", nSamples, LEFT);
 }
 
-// Visualize irradiance samples on a hemisphere.
-function visualizeIrradianceSamples(hemisphereCenterY, sphereRadius, sampleAngularDelta, isUpper) {
-  noStroke();
-  fill(0);
-
+// Function to draw samples on a hemisphere
+function drawHemisphereSamples(sphereRadius, sphereCenterX, hemisphereCenterY, sampleAngularDelta, direction) {
   for (let azimuthalAngle = 0.0; azimuthalAngle < 2.0 * PI; azimuthalAngle += sampleAngularDelta) {
     for (let polarAngle = 0.0; polarAngle < 0.5 * PI; polarAngle += sampleAngularDelta) {
-      // Calculate 3D coordinates on the sphere.
       const x = sin(polarAngle) * cos(azimuthalAngle);
       const y = sin(polarAngle) * sin(azimuthalAngle);
       const z = cos(polarAngle);
 
-      // Rotate the coordinates based on the light source position.
       const rotatedX = cos(lightSourcePosition) * x - sin(lightSourcePosition) * y;
       const rotatedY = sin(lightSourcePosition) * x + cos(lightSourcePosition) * y;
 
-      // Calculate sample position on the canvas.
       const sampleX = rotatedX * sphereRadius + sphereCenterX;
-      const sampleY = hemisphereCenterY + (isUpper ? z - rotatedY * 0.25 : - (z + rotatedY * 0.25)) * sphereRadius;
+      const sampleY = hemisphereCenterY + direction * (z - rotatedY * 0.25) * sphereRadius;
 
-      // Visualize the sample as a small circle.
+      // Draw each sample as a circle
       circle(sampleX, sampleY, 2);
       nSamples++;
     }
