d8fa7bf46e42d6f834a6e95faef167c92d4df39a
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2023-12-29 15:02:28 +0100

    Update Replaceable.js

--- Replaceable.js
+++ Replaceable.js
@@ -1,6 +1,3 @@
-//////////////////////////////////////////////////////////////////////////////////////////////////
-///////// Parameters controlling various aspects of the simulation
-//////////////////////////////////////////////////////////////////////////////////////////////////
 // Frame count modifier (controls the variation speed)
 let frameModifier = 200;
 // Sample density modifier (affects the number of samples)
@@ -11,16 +8,6 @@ let upperHemisphereVerticalAdjustment = 0.95;
 let lowerHemisphereVerticalAdjustment = 1.5;
 // Initial position of the virtual light source
 let lightSourcePosition = 0.0;
-// Fill color for points (modifies point color; currently set to black)
-let pointFillColor = 0;
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Set up canvas based on window dimensions
-///////////////////////////////////////////
-function setup() {
-  const cnv = createCanvas(windowWidth, windowHeight);
-  cnv.id('p5-canvas');
-}
 
 // Creating the visual with draw()
 //////////////////////////////////
@@ -35,14 +22,6 @@ function draw() {
   // Initializing size of the spheres based on screen width:
   // currently set to 0.36 if screen width is less than 600px, otherwise 0.24
   const sphereRadiusPercentage = width < 600 ? 0.36 : 0.24;
-
-  // Set up sphere properties
-  const sphereRadius = min(width, height) * sphereRadiusPercentage;
-  const sphereCenterX = width / 2;
-
-  // Adjusting for vertical separation between spheres
-  const upperHemisphereCenterY = height / 2 - sphereRadius * upperHemisphereVerticalAdjustment;
-  const lowerHemisphereCenterY = height / 2 + sphereRadius * lowerHemisphereVerticalAdjustment;
 
   // Rendering the upper hemisphere
   /////////////////////////////////
@@ -71,87 +50,8 @@ function draw() {
     }
   }
 
-  // Rendering the lower hemisphere
-  /////////////////////////////////
-  // Iterate to cover the entire sphere surface with samples
-  for (let phi = 0.0; phi < 2.0 * PI; phi += sampleAngularDelta) {
-    for (let theta = 0.0; theta < 0.5 * PI; theta += sampleAngularDelta) {
-      // Derive 3D coordinates from spherical angles for incoming light directions
-      const x = sin(theta) * cos(phi);
-      const y = sin(theta) * sin(phi);
-      const z = cos(theta);
-
-      // Rotate the incoming light direction based on the lightSourcePosition
-      const rotatedX = cos(lightSourcePosition) * x - sin(lightSourcePosition) * y;
-      const rotatedY = sin(lightSourcePosition) * x + cos(lightSourcePosition) * y;
-
-      // Calculate the position of each sample point on the lower hemisphere
-      const sampleX = rotatedX * sphereRadius + sphereCenterX;
-      const sampleY = lowerHemisphereCenterY - (z + rotatedY * 0.25) * sphereRadius;
-
-      // Draw the sample point
-      circle(sampleX, sampleY, 2);
-      nSamples++;
-    }
-  }
-
-  // Adjust the position of the virtual light source over time
-  lightSourcePosition += 0.01; // Adjust the increment value based on the desired speed
-  
-  // Display the number of samples taken in the simulation
-  drawLabel(8, 46, "Radiant Reverberations", "Number of samples: ", nSamples, LEFT);
-}
+  // Adjust the position of the virtual light source
+  lightSourcePosition += 0.01;
 
 // Set labels with specified styling, position, and alignment
-/////////////////////////////////////////////////////////////
-function drawLabel(x, y, title, label, value, align = CENTER) {
-  push();
-  strokeWeight(0);
-  textAlign(align);
-  if (align == LEFT) {
-    x += 4;
-  }
-  if (align == RIGHT) {
-    x -= 4;
-  }
-
-  // Font size title 19 if screen width is less than 600px, otherwise 24
-  const titleFontSize = width < 600 ? 19 : 24;
-  // Font size label 15 if screen width is less than 600px, otherwise 20
-  const labelValueFontSize = width < 600 ? 15 : 20;
-
-  // Set up the title of work
-  textStyle(BOLD);
-  fill('#01af52');
-  textFont("Space Mono");
-  textSize(titleFontSize);
-  const titleWidth = textWidth(title);
-  const padding = 3;
-  noStroke();
-  rect(x - padding, y + 45 - titleFontSize + padding / 2, titleWidth + 2 * padding, titleFontSize + padding);
-  fill('#d1d6e6');
-  text(title, x, y + 45);
-
-  // Add a bit more space between the first and second lines
-  const verticalSpace = titleFontSize * 0.2;
-  y += verticalSpace;
-
-  // Set up the static label
-  textStyle(NORMAL);
-  fill('#01af52');
-  textFont("monospace");
-  textSize(labelValueFontSize);
-  text(`${label} `, x, y + 70);
-
-  // Set up the dynamic value of label; currently set to black
-  fill(0);
-  text(value, x + textWidth(label), y + 70);
-
-  pop();
-}
-
-// Function to handle window resizing
-/////////////////////////////////////
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
+//////////////////////////////////////////
