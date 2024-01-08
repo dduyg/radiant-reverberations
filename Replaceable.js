96b8f58420fa756532420ff7f259a8ba50ae2d45
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2024-01-08 22:28:27 +0100

    Update Replaceable.js

--- Replaceable.js
+++ Replaceable.js
@@ -5,25 +5,13 @@ function draw()
   // Parameters influencing sample distribution
   
   // Iterate to cover the entire sphere surface with samples
-  for (let phi = 0.0; phi < 2.0 * PI; phi += sampleAngularDelta) {
-    for (let theta = 0.0; theta < 0.5 * PI; theta += sampleAngularDelta) {
+  for
       // Derive 3D coordinates from spherical angles for incoming light directions
       const x = sin(theta) * cos(phi);
-      ;
-////////////////;
-
+      
+////////////
   // Looping through angular coordinates for sampling
-  for (let azimuthalAngle = 0.0; azimuthalAngle < 2.0 * PI; azimuthalAngle += sampleAngularDelta) {
-    for (let polarAngle = 0.0; polarAngle < 0.5 * PI; polarAngle += sampleAngularDelta) {
-      //
-      );// Loop through azimuthal and polar angles to sample points on the hemisphere
-/// Iterate through the entire spherical surface to cover with samples
-
-// Mapping coordinates to screen space
-      const sampleX = rotatedX
-
-////
- // Iterate through azimuthal and polar angles to sample points on the hemisphere
+ // Iterate through anbgles to sample points on the hemisphere
   for (let
           
       // Draw a small circle at the sample position
