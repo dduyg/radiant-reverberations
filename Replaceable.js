4e781cd239dd09a3025e559283bf780839c965af
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2024-01-08 13:13:05 +0100

    Update Replaceable.js

--- Replaceable.js
+++ Replaceable.js
@@ -12,27 +12,17 @@ function draw()
       const x = sin(theta) * cos(phi);
       const y = sin(theta) * sin(phi);
       const z = cos(theta);
-////////////////_/_//_/_
-
-  // Defining sphere properties
-  const sphereRadiusPercentage = width < 600 ? 0.36 : 0.24;
+////////////////_/_//_/_;
 
   // Calculating hemisphere center positions
   const upperHemisphereCenterY = height / 2 - sphereRadius * upperHemisphereVerticalAdjustment;
-
-  // Updating light source position for animation
-  lightSourcePosition += 0.002;
 
   // Looping through angular coordinates for sampling
   for (let azimuthalAngle = 0.0; azimuthalAngle < 2.0 * PI; azimuthalAngle += sampleAngularDelta) {
     for (let polarAngle = 0.0; polarAngle < 0.5 * PI; polarAngle += sampleAngularDelta) {
       //
-      );
+      );// Loop through azimuthal and polar angles to sample points on the hemisphere
       
       // Mapping coordinates to screen space
       const sampleX = rotatedX * sphereRadius + width / 2;
       const sampleY = hemisphereCenterY + direction * (z - rotatedY * 0.25) * sphereRadius;
-
-      // Rendering the sample point
-      circle(sampleX, sampleY, 2);
-      nSamples++;
