25fc9d8fe5eb64de3e5ace29dd80281725f04b65
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2024-01-08 13:37:34 +0100

    Update Replaceable.js

--- Replaceable.js
+++ Replaceable.js
@@ -13,12 +13,12 @@ function draw()
       const z = cos(theta);
 ////////////////_/_//_/_;
 
-  // Calculating hemisphere center positions
-  const upperHemisphereCenterY = height / 2 - sphereRadius * upperHemisphereVerticalAdjustment;
-
   // Looping through angular coordinates for sampling
   for (let azimuthalAngle = 0.0; azimuthalAngle < 2.0 * PI; azimuthalAngle += sampleAngularDelta) {
     for (let polarAngle = 0.0; polarAngle < 0.5 * PI; polarAngle += sampleAngularDelta) {
       //
       );// Loop through azimuthal and polar angles to sample points on the hemisphere
 /// Iterate through the entire spherical surface to cover with samples
+
+// Mapping coordinates to screen space
+      const sampleX = rotatedX
