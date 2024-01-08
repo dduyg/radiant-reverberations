9b8b9ecf048a7cc412ee0ac919e3bf5e380afdb6
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2024-01-08 15:40:04 +0100

    Update 67.js

--- 67.js
+++ 67.js
@@ -1,30 +1,30 @@
-// Set up the canvas with p5.js
+// Initialize canvas and set its ID
 function setup() {
   const cnv = createCanvas(windowWidth, windowHeight);
   cnv.id('p5-canvas');
 }
 
-// Parameters controlling the dynamics of the simulation
-let frameVariation = 200; // Adjusts the variation in frames
-let sampleDensityModifier = 40; // Controls the density of samples
-let upperHemisphereVerticalAdjustment = 0.95; // Adjusts the vertical position of the upper hemisphere
-let lowerHemisphereVerticalAdjustment = 1.5; // Adjusts the vertical position of the lower hemisphere
-let lightSourcePosition = 0.0; // Controls the position of the virtual light source
+// Parameters influencing the simulation
+let frameVariation = 200; // Rename from frameModifier for clarity
+let sampleDensityModifier = 40;
+let upperHemisphereVerticalAdjustment = 0.95;
+let lowerHemisphereVerticalAdjustment = 1.5;
+let lightSourcePosition = 0.0;
 
-// Main drawing function
+// Main draw function
 function draw() {
   clear();
 
-  // Calculate the number of samples per frame based on parameters
+  // Calculate the number of samples per frame based on variations
   const samplesPerFrame = pow(2, floor((frameCount % frameVariation) / sampleDensityModifier)) * 9;
   const sampleAngularDelta = PI / samplesPerFrame;
 
-  // Calculate sphere dimensions based on canvas size
+  // Set sphere parameters based on canvas size
   const sphereRadiusPercentage = width < 600 ? 0.36 : 0.24;
   const sphereRadius = min(width, height) * sphereRadiusPercentage;
   const sphereCenterX = width / 2;
 
-  // Calculate vertical positions of upper and lower hemispheres
+  // Calculate center positions for upper and lower hemispheres
   const upperHemisphereCenterY = height / 2 - sphereRadius * upperHemisphereVerticalAdjustment;
   const lowerHemisphereCenterY = height / 2 + sphereRadius * lowerHemisphereVerticalAdjustment;
 
@@ -43,13 +43,12 @@ function draw() {
 function renderHemisphereSamples(hemisphereCenterY, sphereRadius, sampleAngularDelta, direction) {
   noStroke();
   fill(0);
-  
+
   let nSamples = 0;
 
-  // Loop through azimuthal and polar angles to sample points on the hemisphere
+  // Iterate through azimuthal and polar angles to sample points on the hemisphere
   for (let azimuthalAngle = 0.0; azimuthalAngle < 2.0 * PI; azimuthalAngle += sampleAngularDelta) {
     for (let polarAngle = 0.0; polarAngle < 0.5 * PI; polarAngle += sampleAngularDelta) {
-
       // Convert spherical coordinates to Cartesian coordinates
       const x = sin(polarAngle) * cos(azimuthalAngle);
       const y = sin(polarAngle) * sin(azimuthalAngle);
@@ -68,52 +67,6 @@ function renderHemisphereSamples(hemisphereCenterY, sphereRadius, sampleAngularD
       nSamples++;
     }
   }
-  
-  return nSamples;
-}
-
-// Function to display project information
-function displayInfo(x, y, title, label, value, align = CENTER) {
-  push();
-  strokeWeight(0);
-  textAlign(align);
-  if (align == LEFT) {
-    x += 4;
-  }
-  if (align == RIGHT) {
-    x -= 4;
-  }
-
-  // Styling for project title
-  const titleFontSize = width < 600 ? 19 : 24;
-  const labelValueFontSize = width < 600 ? 17 : 22;
-
-  textStyle(BOLD);
-  fill('#01af52');
-  textFont("Space Mono");
-  textSize(titleFontSize);
-  const titleWidth = textWidth(title);
-  const padding = 3;
-  noStroke();
-  rect(x - padding, y + 45 - titleFontSize + padding / 2, titleWidth + 2 * padding, titleFontSize + padding);
-  fill('#d1d6e6');
-  text(title, x, y + 45);
-
-  // Styling for label and value
-  const verticalSpace = titleFontSize * 0.3;
-  y += verticalSpace;
-
-  textStyle(NORMAL);
-  fill('#01af52');
-  textFont("monospace");
-  textSize(labelValueFontSize);
-  text(`${label} `, x, y + 70);
-
-  fill(0);
-  text(value, x + textWidth(label), y + 70);
-
-  pop();
-}
 
 // Adjust canvas size when window is resized
 function windowResized() {
